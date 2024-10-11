e139ef02339ec066219a2ded2b86330a85d5c9a2
NurikD <duishoev0123@gmail.com>
2024-10-11 15:55:08 +0500

    Убрал код и вместо этого вывел просто привет

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-#Получаем глассные
-# Этот пример возвращает в строке найденные гласные "a e i o u".
-# Это может оказаться полезным при поиске или обнаружении гласных.
-def get_vowels(String):
-    return [each for each in String if each in "aeiou"]
-get_vowels("animal") # [a, i, a]                       
-get_vowels("sky") # []
-get_vowels("football") # [o, o, a]
+print ("Hello")
