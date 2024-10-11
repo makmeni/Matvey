d4c0894dc3adaf96087804397bd7db9f04ec8a80
NurikD <duishoev0123@gmail.com>
2024-10-11 16:08:38 +0500

    Код на получение гласных, проверка дубликата

---
+++ 1.py
@@ -0,0 +1,7 @@
+#Код получение гласных которое возвращает в строке найденные гласные
+
+def get_vowels(String):
+    return [each for each in String if each in "aeiou"]
+get_vowels("animal") # [a, i, a]
+get_vowels("sky") # []
+get_vowels("football") # [o, o, a]
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print ("Hello")
+def main():
+    print(f"Hi, My first git programm")
+
+
+if __name__ == '__main__':
+    main()
